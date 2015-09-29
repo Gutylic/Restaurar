21a2bb4a170505212de9b52973856df92ae5053e
gusty <gutylic@gmail.com>
2015-09-29 03:43:53 -0300

    Inicial 29/9

---
+++ Restaurar/index.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Restaurar
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Boton_1_Click(object sender, EventArgs e)
+        {
+            if (Valor_1.Text == "gustavo" && Valor_2.Text =="123456")
+            {
+                Boton_2.Enabled = true;
+                Boton3.Enabled = true;
+            }
+        }
+
+        protected void Boton_2_Click(object sender, EventArgs e)
+        {
+            DataClassesDataContext db = new DataClassesDataContext();
+            db.Restaurar();
+            Boton_2.Enabled = false;
+            string alerta = @"alert('BackUp creado correctamente');"; 
+                
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "", alerta, true);
+
+            return;
+        }
+
+        protected void Boton3_Click(object sender, EventArgs e)
+        {
+            DataClasses1DataContext db1 = new DataClasses1DataContext();
+            db1.Limpiador_Back_UP();
+            Boton3.Enabled = false;
+            string alerta = @"alert('Base de Datos limpia de procedimientos');";
+
+            ScriptManager.RegisterStartupScript(this, typeof(Page), "", alerta, true);
+
+            return;
+        }
+
+
+
+    }
+}
---
+++ Restaurar/index.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Restaurar {
+    
+    
+    public partial class index {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control Valor_1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Valor_1;
+        
+        /// <summary>
+        /// Control Valor_2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Valor_2;
+        
+        /// <summary>
+        /// Control Boton_1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Boton_1;
+        
+        /// <summary>
+        /// Control Boton_2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Boton_2;
+        
+        /// <summary>
+        /// Control Boton3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Boton3;
+    }
+}
